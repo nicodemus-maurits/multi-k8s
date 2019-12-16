a6640d36a72bb5a09eebe8bf11d18dc43ec99d96
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-12-16 23:12:50 +0700

    Removed git submodule

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+import Fib from './Fib'
+import OtherPage from './OtherPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to='/'>Home</Link>
+          <Link to='/otherpage'>Other Page</Link>
+        </header>
+        <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+      </div>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
